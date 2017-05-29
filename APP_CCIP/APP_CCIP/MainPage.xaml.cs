af782e7143d40cd82e2c9e67678926619070f5fa
Leandro Costa <leandro.mbccosta@gmail.com>
2017-05-29 21:31:38 +0100

    M_CCIP_APK
    M_CCIP_APK

---
+++ APP_CCIP/APP_CCIP/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace APP_CCIP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.Source = "http://www.m.ccip.pt/";
+        }
+    }
+}
